726eb2ca78dc9798357caaaf4abc6c4cf02f31c7
Iago <iagojlourenco@gmail.com>
2021-06-17 00:54:36 -0300

    More binaries

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ sudo apt-get upgrade -y
 # Android Studio dependencies
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 
-echo "Installing tools: (git,curl,wget)"
-sudo apt-get install git curl wget -y
+echo "Installing tools: (git,curl,wget,htop)"
+sudo apt-get install git curl wget htop -y
 
 echo "Installing Snap"
 sudo apt-get install snapd -y
@@ -34,6 +34,26 @@ sudo apt-get update && sudo apt-get install spotify-client -y
 echo "Installing VS Code"
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 sudo apt-get install ./*.deb -y
+rm -f *.deb
+
+echo "Installing Discord"
+wget https://discord.com/api/download?platform=linux&format=deb
+sudo apt-get install ./*.deb
+rm -f *.deb
+
+echo "Installing GitKraken"
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo apt-get install ./*.deb
+rm -f *.deb
+
+echo "Installing Beekeeper Studio"
+wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
+echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+sudo apt update && sudo apt install beekeeper-studio -y
+
+echo "Installing Insomnia"
+echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+sudo apt-get update && sudo apt-get install insomnia
 
 echo "Installing ffmpeg"
 sudo snap install ffmpeg
